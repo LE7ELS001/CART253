00a0adbfd907f9d57cdf5b38a8189d7962ce353e
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2024-09-13 17:51:38 -0400

    Try the bird's move

--- Topics/3.Variables/Challenges/Challenges/js/script.js
+++ Topics/3.Variables/Challenges/Challenges/js/script.js
@@ -10,7 +10,7 @@
 let mrFurious = {
     // Position and size
     x: 200,
-    y: 200,
+    y: 300,
     size: 100,
     // Colour
     fill: {
@@ -29,6 +29,37 @@ let sky = {
     redReduce: -0.5,
     greenReduce: -0.5,
     BlueReduce: -0.5
+}
+
+//bird
+let bird = {
+    red: 0,
+    green: 255,
+    blue: 100,
+    x: 20,
+    y: 150,
+    size: 20,
+
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    acceleration: {
+        x: 0.05,
+        y: 0.01
+    },
+    maxSpeed: 2,
+    minSpeed: -2,
+    flyingRange: {
+        maxHorizontal: 370,
+        minHorizontal: 20,
+        maxVertical: 220,
+        minVertical: 120
+    },
+    isReachTheEdgeX: false,
+    isReachTheEdgeY: false,
+    yPullUp: 0.5
+
 }
 
 /**
@@ -59,9 +90,74 @@ function draw() {
     sky.green = constrain(sky.green, 0, 255);
     sky.blue = constrain(sky.blue, 0, 255);
 
-    console.log(sky.red);
-    console.log(sky.green);
-    console.log(sky.blue);
+    // console.log(sky.red);
+    // console.log(sky.green);
+    // console.log(sky.blue);
+
+    //bird 
+    push()
+    noStroke();
+    fill(bird.red, bird.green, bird.blue);
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
+
+    //bird flying 
+    bird.velocity.x += bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+    console.log(bird.velocity.y);
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minSpeed, bird.maxSpeed);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minSpeed, bird.maxSpeed);
+
+    bird.x += bird.velocity.x;
+    bird.y += bird.velocity.y;
+
+    bird.x = constrain(bird.x, bird.flyingRange.minHorizontal, bird.flyingRange.maxHorizontal);
+    bird.y = constrain(bird.y, bird.flyingRange.minVertical, bird.flyingRange.maxVertical);
+
+
+    if (bird.x == 370 || bird.x == 20) {
+        bird.isReachTheEdgeX = true;
+        //bird.x += bird.velocity.x;
+
+    }
+
+
+    if (bird.y == 200 || bird.y == 70) {
+        bird.isReachTheEdgeY = true;
+        console.log("ativeY true");
+        bird.yPullUp = -bird.yPullUp;
+        //bird.y -= bird.acceleration.y;
+    }
+
+
+
+
+    if (bird.isReachTheEdgeX == true) {
+        bird.velocity.x = 0;
+        bird.acceleration.x = -bird.acceleration.x;
+        //bird.acceleration.x *= -1;
+        bird.isReachTheEdgeX = false;
+    }
+
+    if (bird.isReachTheEdgeY == true) {
+        bird.velocity.y = 0;
+        bird.acceleration.y = -bird.acceleration.y;
+        bird.y += bird.yPullUp;
+
+        bird.isReachTheEdgeY == false;
+
+    }
+
+
+
+    //console.log(bird.x);
+    console.log(bird.y);
+    //console.log(bird.acceleration.x);
+    console.log(bird.acceleration.y);
+    //console.log(bird.velocity.x);
+    console.log(bird.velocity.y);
+
 
     // Draw Mr. Furious as a coloured circle
     push();
