f5f6ce1bff598b7250179fd649e4b9cfdc4ee2c6
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2024-09-13 16:04:14 -0400

    Finish the second challenge

--- Topics/3.Variables/Challenges/Challenges/js/script.js
+++ Topics/3.Variables/Challenges/Challenges/js/script.js
@@ -21,6 +21,16 @@ let mrFurious = {
     greenReduce: -0.5,
     BlueReduce: -0.5
 };
+//sky
+let sky = {
+    red: 160,
+    green: 180,
+    blue: 200,
+    redReduce: -0.5,
+    greenReduce: -0.5,
+    BlueReduce: -0.5
+}
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
@@ -33,11 +43,25 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-    background(160, 180, 200);
+    background(sky.red, sky.green, sky.blue);
+
+    //MrFurious turns red
     mrFurious.fill.g += mrFurious.greenReduce;
     mrFurious.fill.b += mrFurious.BlueReduce;
     mrFurious.fill.g = constrain(mrFurious.fill.g, 20, 255);
     mrFurious.fill.b = constrain(mrFurious.fill.b, 20, 255);
+
+    //Sky(background) turns dark
+    sky.red += sky.redReduce;
+    sky.green += sky.greenReduce;
+    sky.blue += sky.BlueReduce;
+    sky.red = constrain(sky.red, 0, 255);
+    sky.green = constrain(sky.green, 0, 255);
+    sky.blue = constrain(sky.blue, 0, 255);
+
+    console.log(sky.red);
+    console.log(sky.green);
+    console.log(sky.blue);
 
     // Draw Mr. Furious as a coloured circle
     push();
