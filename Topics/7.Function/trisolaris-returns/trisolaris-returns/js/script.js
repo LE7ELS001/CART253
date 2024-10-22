0d61d2e74ac0f1368c22025ed653e72dba270604
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2024-10-22 09:18:47 -0400

    Finish return value function

---
+++ Topics/7.Function/trisolaris-returns/trisolaris-returns/js/script.js
@@ -0,0 +1,51 @@
+/**
+ * Trisolaris Returns (get it?)
+ * Pippin
+ * 
+ * Draws the three suns of Trisolaris. Calculate their colour
+ * based on their parameters.
+ */
+
+"use strict";
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(600, 400);
+}
+
+/**
+ * Draw the three suns
+ */
+function draw() {
+    // Sky blue
+    background("#87ceeb");
+
+    drawSun(500, 100, 80);
+    drawSun(350, 180, 200);
+    drawSun(120, 100, 160);
+}
+
+/**
+ * Draws a Trisolarian sun
+ */
+function drawSun(x, y, size) {
+
+    let weight = calculateStrokeWeight(x, y);
+
+    push();
+    strokeWeight(weight);
+    stroke("#ffff00");
+    fill("#f99736");
+    ellipse(x, y, size);
+    pop();
+}
+
+function calculateStrokeWeight(x, y) {
+    const minWeight = 20;
+    const maxWeight = 1;
+    let d = dist(mouseX, mouseY, x, y);
+    let result = map(d, 0, width, minWeight, maxWeight);
+    return result;
+}
