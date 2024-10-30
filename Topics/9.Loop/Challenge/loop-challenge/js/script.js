1006223e72cf941c80b6ba1db2a60cd9614c2978
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2024-10-30 14:30:34 -0400

    Finish loop-challenge

--- Topics/9.Loop/Challenge/loop-challenge/js/script.js
+++ Topics/9.Loop/Challenge/loop-challenge/js/script.js
@@ -7,8 +7,14 @@
 
 "use strict";
 
+let backgroundColor = 0;
+let startBackgroundColor = 0;
+let colorRange = 100;
+
 let startX = 0;
 let originX = 0;
+let startY = 0;
+let originY = 0;
 let gap = 50;
 
 let startColor = 0;
@@ -24,6 +30,7 @@ let lineWeightGap = 1;
  */
 function setup() {
     createCanvas(500, 500);
+
 }
 
 /**
@@ -32,74 +39,65 @@ function setup() {
  */
 function draw() {
 
-
     background("pink");
+
+
+    colorMode(HSB);
+    for (let i = 0; i < height; i++) {
+        stroke(i, i, colorRange);
+        line(0, i, width, i)
+    }
+
+
+
+
 
 
 
     while (originX <= width) {
 
+        //vertical
         push();
         stroke(originColor);
         strokeWeight(OriginLineWeight);
         line(originX, 0, originX, height);
         pop();
 
-        console.log(originX, originColor, OriginLineWeight);
+        //horizontal
+        push();
+        stroke(originColor);
+        strokeWeight(OriginLineWeight);
+        line(0, originY, width, originY);
+        pop();
 
+
+        //debug
+        //console.log(originX, originColor, OriginLineWeight);
 
         originX += gap;
+        originY += gap;
         originColor += colorGap;
         OriginLineWeight += lineWeightGap;
 
+
     }
 
+    resetValue();
+
+
+
+
+
+
+
+
+
+}
+
+function resetValue() {
     originX = startX;
+    originY = startY
     originColor = startColor;
     OriginLineWeight = startLineWeight;
 
-
-    // stroke(0);
-    // strokeWeight(1);
-    // line(0, 0, 0, height);
-
-    // stroke(25);
-    // strokeWeight(2);
-    // line(50, 0, 50, height);
-
-    // stroke(50);
-    // strokeWeight(3);
-    // line(100, 0, 100, height);
-
-    // stroke(75);
-    // strokeWeight(4);
-    // line(150, 0, 150, height);
-
-    // stroke(100);
-    // strokeWeight(5);
-    // line(200, 0, 200, height);
-
-    // stroke(125);
-    // strokeWeight(6);
-    // line(250, 0, 250, height);
-
-    // stroke(150);
-    // strokeWeight(7);
-    // line(300, 0, 300, height);
-
-    // stroke(175);
-    // strokeWeight(8);
-    // line(350, 0, 350, height);
-
-    // stroke(200);
-    // strokeWeight(9);
-    // line(400, 0, 400, height);
-
-    // stroke(225);
-    // strokeWeight(10);
-    // line(450, 0, 450, height);
-
-    // stroke(250);
-    // strokeWeight(11);
-    // line(500, 0, 500, height);
 }
