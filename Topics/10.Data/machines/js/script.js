7070cc544db1f059d5b1be6ee8ce584b14bada0a
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2024-11-12 08:55:19 -0500

    finish first course of data study

---
+++ Topics/10.Data/machines/js/script.js
@@ -0,0 +1,63 @@
+/**
+ * Machines
+ * Pippin Barr
+ * 
+ * A starting point for a project that displays different machines
+ * on the canvas. Eventually I'd like to be able to drag and drop
+ * items onto the machines and have them act in different ways.
+ * For now I'm happy to just show that they're distinct.
+ */
+
+"use strict";
+const machineWidth = 100;
+const machineHeight = 100;
+
+let machines = [
+    {
+        type: "incinerator",
+        x: 0,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#ff4400",
+    },
+
+    {
+        type: "freezer",
+        x: 150,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#bbbbff",
+    },
+
+    {
+        type: "crusher",
+        x: 300,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#777777",
+    }
+];
+
+function setup() {
+    createCanvas(400, 200);
+}
+
+
+function draw() {
+    background(0);
+
+    for (let machine of machines) {
+        drawMachine(machine);
+    }
+}
+
+function drawMachine(machine) {
+    push();
+    noStroke();
+    fill(machine.fill);
+    rect(machine.x, machine.y, machine.width, machine.height);
+    pop();
+}
